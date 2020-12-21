d95e95e0e1d3aec24cc4c3642879d671444be652
ScottyMcPiper <67666425+ScottyMcPiper@users.noreply.github.com>
2020-12-21 00:04:48 -0800

    Update BodyMoverExamples.lua

--- Chapter04/BodyMoverExamples.lua
+++ Chapter04/BodyMoverExamples.lua
@@ -1,13 +1,16 @@
 local part = workspace.Part
+
 local bodyForce = Instance.new("BodyForce")
 bodyForce.Parent = part
+
 local requiredForce = part:GetMass() * workspace.Gravity
 bodyForce.Force = Vector3.new(0,requiredForce,0)
 
 
 local Part2 = workspace.Part2
 local lookVector = (Part2.Position - Part1.Position).Unit
---p2 – p1 = directional vector at p1 looking at p2
+--p2 â€“ p1 = directional vector at p1 looking at p2
+
 local moverVel = lookVector * projectileVel
 bodyVelocity.Velocity = moverVel
 
